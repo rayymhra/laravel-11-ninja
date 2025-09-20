a6c941cd6fbb862ef553ea2ad9a6eab51cfdc22e
rayymhra <rayraeilish@gmail.com>
2025-09-20 20:18:27 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to MLM Network</h1>
+    <p>Click the button to see people in mlm fields</p>
+    <a href="" class="btn btn-primary">Find People</a>
+</body>
+</html>
